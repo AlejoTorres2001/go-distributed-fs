5b9716e83d61c2a27207b0ab26150c0938f173b3
AlejoTorres2001 <torres.alejo2001@gmail.com>
2025-05-05 18:58:07 -0300

    Refactor main function to use TCPTransportOpts for improved configuration

--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import "github.com/AlejoTorres2001/go-distributed-fs/p2p"
 
-
 func main() {
-	tr := p2p.NewTCPTransport(":8080")
+	tcpopts := p2p.TCPTransportOpts{
+		ListenAddress: ":8080",
+		HandshakeFunc:     p2p.NOPHandshakeFunc,
+		Decoder:       p2p.DefaultDecoder{},
+	}
+	tr := p2p.NewTCPTransport(tcpopts)
 
 	if err := tr.ListenAndAccept(); err != nil {
 		panic(err)
